f0127410f82ec860f1a3419a265cfe4edde7630d
Luke0195 <lucas201273@gmail.com>
2020-10-28 09:54:28 -0300

    chore: add routes

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'Vamos para o próximo nivel ' }));
+
+app.listen(3331, () => {
+  console.log('O servidor está rodando na porta localhost:3331');
+});
