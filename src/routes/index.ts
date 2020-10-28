23583bcc1367946f43a2ff4120971ad1492f26dc
Luke0195 <lucas201273@gmail.com>
2020-10-28 10:09:32 -0300

    prettier config eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const routes = Router();
 
-routes.get('/teste', (request, response) => response.json({ message: 'Vamos para o próximo nivel ' }));
+routes.get("/teste", (request, response) =>
+  response.json({ message: "Vamos para o próximo nivel " })
+);
 
 export default routes;
